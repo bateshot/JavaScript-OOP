1df8ef07d38afc3253c1a1c7dfea82dd429b9fd9
bateshot <mac@macs-MacBook-Pro.local>
2015-06-19 18:51:33 +0300

    homework submission
    closures & scope HW

--- 02. Scopes and Closures/homework/tasks/task-1.js
+++ 02. Scopes and Closures/homework/tasks/task-1.js
@@ -24,18 +24,73 @@ function solve() {
 	var library = (function () {
 		var books = [];
 		var categories = [];
+
 		function listBooks() {
+			var result = [],
+				filter = '',
+				aut = '';
+			//Argument provided:
+			if (arguments.length != 0){
+
+				//Category provided:
+				if(arguments[0].hasOwnProperty('category')) {
+					filter = arguments[0].category;
+					books.forEach(function(item){
+						if(item.category == filter){
+							result.push(item);
+						}
+					});
+					return result;
+				}
+				//Author provided:
+				if(arguments[0].hasOwnProperty('author')) {
+					filter = arguments[0].author;
+					books.forEach(function(item){
+						if(item.author == filter){
+							result.push(item);
+						}
+					});
+					return result;
+				}
+			}
 			return books;
 		}
 
 		function addBook(book) {
+			book = validateBook(book);
 			book.ID = books.length + 1;
 			books.push(book);
+			if(categories.length > 0){
+				if(categories.indexOf(book.category) == -1){
+					categories.push(book.category);
+				}
+			} else {
+				categories.push(book.category);
+			}
+
 			return book;
 		}
 
 		function listCategories() {
 			return categories;
+		}
+
+		function validateBook(x) {
+			//Author
+			if(typeof x.author !== 'string' || x.author.length =='') throw 'Error';
+			//Name
+			if(x.title.length < 2 || x.title.length > 100) throw 'Error';
+			if(books.length > 0){
+				if(books.some(function(element){ return element.title == x.title;})) throw 'Error';
+			}
+			//isbn
+			if(!(x.isbn.length == 13 || x.isbn.length == 10)) throw 'Error';
+			if(isNaN(x.isbn)) throw 'Error';
+			if(books.length > 0){
+				if(books.some(function(element){ return element.isbn == x.isbn;})) throw 'Error';
+			}
+
+			return x;
 		}
 
 		return {
